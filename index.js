a4d79127ef9af027675ae77f84def7f5f3eedbcd
Hassan Farooq <1hassanfarooq@gmail.com>
2020-03-04 17:17:16 -0600

    updated index

--- index.js
+++ index.js
@@ -14,21 +14,24 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`ggwtf help`);
 });
 
 // Runs on join new server (?)
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`ggwtf help`);
 });
 
 // Runs on leave server (?)
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`ggwtf help`);
 });
 
 
@@ -42,7 +45,9 @@ client.on("message", async message => {
   const command = message.content.toLowerCase();
 
   // All the cases
-  if(command.includes("getwtfcounter")) {
+  if(command.includes("ggwtf help")) {
+    message.channel.send("Commands:\n'getWtfCounter' to get the current counter across all servers");
+  } else if(command.includes("getwtfcounter")) {
     // Get counter
     message.channel.send("gg wtf count: ".concat(count[0]["messageCount"], "\ngg count: ", count[1]["messageCount"].toString(), "\nwtf count: ", count[2]["messageCount"].toString()));
   } else if(command.includes("gg") && command.includes("wtf")) {
