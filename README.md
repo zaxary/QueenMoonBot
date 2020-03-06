2553e71d6488582d62006452b81d5b067282ba6b
Hassan Farooq <1hassanfarooq@gmail.com>
2020-03-05 22:45:35 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Killamocingbot
 
-Killa but as a Discord bot 
+Killa but as a Discord bot
 
 ## Add it to your server:
   https://discordapp.com/oauth2/authorize?&client_id=684867671552294994&scope=bot&permissions=8
 
 
+## Commands
+By default, the `wtf`, `gg`, `wtf gg`, `b o g`, and `when` commands only have a 33% chance of appearing. This can be overridden by adding `killa` to the beginning of your message. Other commands, such as `killa help`, `killa usercount`, `killa no anime`, `killa hackathon`, and `getKillaCounter` appear every time a message with one of those commands is sent.
 
+#### Other commands
 
-## To run on your own server
+* `getKillaCounter` to get the current counter across all servers
+* add `killa` to the start of your message to override the 33% chance of the bot responding
+* `killa no anime` to get the no anime picture
+* `killa hackathon` to get the done with hackathons picture
+* `killa usercount` to see how many users are currently in the server
+
+## To run the bot yourself
 make a file named `config.json` in the root directory of the repo. Head over to the [Discord Developer Portal](https://discordapp.com/developers/applications/me) and get a token for a new bot. Place that token in `config.json` in the following format:
 
 ```
--- index.js
+++ index.js
@@ -8,27 +8,19 @@ const client = new Discord.Client();
 
 // Run on start
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
   client.user.setActivity(`killa help`);
 });
 
-// Runs on join new server (?)
+// Runs on join new server
 client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
   client.user.setActivity(`killa help`);
 });
 
-// Runs on leave server (?)
+// Runs on leave server
 client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
   client.user.setActivity(`killa help`);
 });
 
@@ -69,7 +61,7 @@ client.on("message", async message => {
     override = true;
 
   if(command.match(/\bkilla help\b/) != null) {
-    message.channel.send("Commands:\n```* `getKillaCounter` to get the current counter across all servers\n* add `killa` to the start of your message to override the 33% chance of the bot responding\n* `killa no anime` to get the no anime picture\n* `killa hackathon` to get the done with hackathons picture\n* `killa usercount` to see how many users are in the server currently```");
+    message.channel.send("Commands:\n```* `getKillaCounter` to get the current counter across all servers\n* add `killa` to the start of your message to override the 33% chance of the bot responding\n* `killa no anime` to get the no anime picture\n* `killa hackathon` to get the done with hackathons picture\n* `killa usercount` to see how many users are currently in the server```");
   } else if(command.match(/\bgetkillacounter\b/) != null) {
     message.channel.send("gg wtf count: ".concat(count[0]["messageCount"], "\ngg count: ", count[1]["messageCount"].toString(), "\nwtf count: ", count[2]["messageCount"].toString(), "\nb o g count: ", count[3]["messageCount"].toString(), "\neda count: ", count[4]["messageCount"].toString()));
   } else if(command.match(/\busercount\b/) != null) {
