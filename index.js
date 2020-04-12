b793803831e30c62fe0cc5715bcab954cc0a6a0b
Hassan Farooq <1hassanfarooq@gmail.com>
2020-04-11 18:03:12 -0700

    8ball

--- index.js
+++ index.js
@@ -27,7 +27,28 @@ client.on("guildDelete", guild => {
 // Runs when a new message is sent on a server
 client.on("message", async message => {
   // Counting game stuff
-  if(message.channel.id === '654783232969277453') {
+  var responses = ['It is certain.',
+            'It is decidedly so.',
+            'Without a doubt.',
+            'Yes - definitely.',
+            'You may rely on it.',
+            'As I see it, yes.',
+            'Most likely.',
+            'Outlook good.',
+            'Yes.',
+            'Signs point to yes.',
+            'Reply hazy, try again.',
+            'Ask again later.',
+            'Better not tell you now.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            "Don't count on it.",
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good',
+            'Very doubtful'];
+
+  if(message.channel.id === '698313651186040923') {
       message.channel.messages.fetch({ limit: 2 }).then(messages => {
 
         // Delete bot messages
@@ -85,7 +106,11 @@ client.on("message", async message => {
       message.channel.send("https://cdn.discordapp.com/attachments/654784388197908500/675113678856781834/Screenshot_20200102-213727_Discord.png");
       return;
     } else if(command.match(/\bhelp\b/) != null) {
-      message.channel.send("Commands:\n```* `queen hackathon` to get the done with hackathons picture\n* `queen usercount` to see how many users are currently in the server```");
+      message.channel.send("Commands:\n```* `queen hackathon` to get the done with hackathons picture\n* `queen usercount` to see how many users are currently in the server\n* `queen 8ball [message]` to get an 8ball reply```");
+    } else if(command.match(/\b8ball\b/) != null) {
+      var rand = Math.floor(Math.random() * 20);
+      var question =
+      message.channel.send("Question: " + message.content.substring(12) + "\nAnswer: " + responses[rand]);
     }
   }
 });
