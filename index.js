d515e6e76a5dc6db5aff3af159956f451f49da1b
Hassan Farooq <1hassanfarooq@gmail.com>
2020-05-01 15:52:57 -0700

    auto role

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const count = require('./count.json');
 const config = require("./config.json");
 
-const client = new Discord.Client();
+const client = new Discord.Client({
+  partials: ['MESSAGE']
+});
 
 // Run on start
 client.on("ready", () => {
@@ -24,8 +26,84 @@ client.on("guildDelete", guild => {
   client.user.setActivity(`queen help`);
 });
 
+
+let announcements_channel_id = "654785609335832586";
+let committed_id = "683065503300059179";
+let admitted_id = "683067243566858250";
+let prospective_id = "654786754657910815";
+let traitor_id = "696956653844561931";
+let bot_channel_id = "654783232969277453";
+
+
+client.on("messageReactionAdd",(reaction,user)=>{
+  console.log("START REACT");
+
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  if(!reaction.message.channel.id === announcements_channel_id) return;
+  member = reaction.message.guild.member(user)
+  console.log(member.id);
+  if (!member.roles.cache.some(r => r.name === "Prospective Student")  && !member.roles.cache.some(r => r.name === "Admitted Student")){
+    
+    return;
+  }
+
+  let committed_role = member.guild.roles.cache.find(r => r.id === committed_id);
+  let admitted_role = member.guild.roles.cache.find(r => r.id === admitted_id);
+  let prospective_role = member.guild.roles.cache.find(r => r.id === prospective_id);
+  let traitor_role = member.guild.roles.cache.find(r => r.id === traitor_id);
+
+  console.log("USER CREDENTIALS PASS");
+  if(reaction.emoji.name == 'blockI') {
+    console.log("blocki react");
+    member.roles.add(committed_role);   
+
+    member.roles.remove(admitted_role).then(console.log).catch(console.error);
+    member.roles.remove(prospective_role).then(console.log).catch(console.error);
+
+    // if (!member.roles.cache.some(r => r.id === "654828722050039808") && 
+    //     !member.roles.cache.some(r => r.id === "654828844133384222") && 
+    //     !member.roles.cache.some(r => r.id === "654828941072400393") && 
+    //     !member.roles.cache.some(r => r.id === "655176104251883561")) {
+    //   member.user.send("Don't forget to request your roles in #set_roles_here!\n**ex: Admitted, Grainger, OOS, ElecEng @ESC**").catch(console.error);
+    //   return;
+    // }
+  }
+  if(reaction.emoji.name == 'traitor'){
+    console.log("traitor react");
+
+    member.roles.add(traitor_role);
+    member.roles.remove(admitted_role).then(console.log).catch(console.error);
+    member.roles.remove(prospective_role).then(console.log).catch(console.error);
+  }
+});
+
 // Runs when a new message is sent on a server
 client.on("message", async message => {
+
+  var responses = ['It is certain.',
+            'It is decidedly so.',
+            'Without a doubt.',
+            'Yes - definitely.',
+            'You may rely on it.',
+            'As I see it, yes.',
+            'Most likely.',
+            'Outlook good.',
+            'Yes.',
+            'Signs point to yes.',
+            'Reply hazy, try again.',
+            'Ask again later.',
+            'Better not tell you now.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            "Don't count on it.",
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good',
+            'Very doubtful'];
+
+
   // Counting game stuff
   if(message.channel.id === '698313651186040923') {
       message.channel.messages.fetch({ limit: 2 }).then(messages => {
@@ -68,6 +146,7 @@ client.on("message", async message => {
 
 
   const command = message.content.toLowerCase();
+
   let override = false;
 
   if (command.startsWith("queen"))
@@ -89,10 +168,10 @@ client.on("message", async message => {
       return;
     } else if(command.match(/\bhelp\b/) != null) {
       message.channel.send("Commands:\n```* `queen hackathon` to get the done with hackathons picture\n* `queen gc` to get the Facebook group screenshot\n* `queen usercount` to see how many users are currently in the server\n* `queen 8ball [message]` to get an 8ball reply (only works in #spam)```");
-    } else if(command.match(/\b8ball\b/) != null && message.channel.id === '654838387160907777') {
+    } else if(command.match(/\b8ball\b/) != null) {
       var rand = Math.floor(Math.random() * 20);
-      var question = message.channel.send("Question: " + message.content.substring(12) + "\nAnswer: " + responses[rand]);
-      message.channel.send("Commands:\n```* `queen hackathon` to get the done with hackathons picture\n* `queen usercount` to see how many users are currently in the server```");
+      var question =
+      message.channel.send("Question: " + message.content.substring(12) + "\nAnswer: " + responses[rand]);
     }
   }
 });
