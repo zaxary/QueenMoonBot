e175c5aefe15e5615ffac000e9bae636c7594a1a
Hassan Farooq <1hassanfarooq@gmail.com>
2020-05-05 19:36:42 -0700

    because Gloria is special

--- index.js
+++ index.js
@@ -125,8 +125,11 @@ client.on("message", async message => {
         if(Number(lastMessage[0].content).toString() !== lastMessage[0].content || Number(lastMessage[0].content) - 1 != Number(lastMessage[1].content)) {
           // Add Can't Count role and delete last message if number isn't next in counting game and > 1500 ms between the two messages
           if(Math.abs(lastMessage[0].createdTimestamp - lastMessage[1].createdTimestamp) > 1500) {
-            const tMember = lastMessage[0].member.guild.roles.cache.find(role => role.name === "Can't Count");
-            lastMessage[0].member.roles.add(tMember).catch(console.error);
+            // Because Gloria is special
+            if(lastMessage[0].member.id !== '242067290479132675') {
+              const tMember = lastMessage[0].member.guild.roles.cache.find(role => role.name === "Can't Count");
+              lastMessage[0].member.roles.add(tMember).catch(console.error);
+            }
           }
 
           message.delete(lastMessage[0]);
@@ -169,9 +172,10 @@ client.on("message", async message => {
     } else if(command.match(/\bhelp\b/) != null) {
       message.channel.send("Commands:\n```* `queen hackathon` to get the done with hackathons picture\n* `queen gc` to get the Facebook group screenshot\n* `queen usercount` to see how many users are currently in the server\n* `queen 8ball [message]` to get an 8ball reply (only works in #spam)```");
     } else if(command.match(/\b8ball\b/) != null) {
-      var rand = Math.floor(Math.random() * 20);
-      var question =
-      message.channel.send("Question: " + message.content.substring(12) + "\nAnswer: " + responses[rand]);
+      if(message.channel.id === '654838387160907777') {
+        var rand = Math.floor(Math.random() * 20);
+        message.channel.send("Question: " + message.content.substring(12) + "\nAnswer: " + responses[rand]);
+      }
     }
   }
 });
