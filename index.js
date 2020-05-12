a4e2986c4121b70bccd48efe50891e4048437b15
Hassan F <1hassanfarooq@gmail.com>
2020-05-12 13:45:25 -0700

    formatted

--- index.js
+++ index.js
@@ -84,24 +84,26 @@ async function getMessagesWithImages(channel, limit = 500) {
   let last_id;
 
   while (true) {
-      const options = { limit: 100 };
-      if (last_id) {
-          options.before = last_id;
-      }
+    const options = {
+      limit: 100
+    };
+    if (last_id) {
+      options.before = last_id;
+    }
 
-      const messages = await channel.messages.fetch(options);
-      sum_messages.push(...messages.array());
-      last_id = messages.last().id;
+    const messages = await channel.messages.fetch(options);
+    sum_messages.push(...messages.array());
+    last_id = messages.last().id;
 
-      if (messages.size != 100 || sum_messages >= limit) {
-          break;
-      }
+    if (messages.size != 100 || sum_messages >= limit) {
+      break;
+    }
   }
 
 
   const output = [];
-  for(let i = 0; i < sum_messages.length; i++) {
-    if(sum_messages[i].attachments.size > 0)
+  for (let i = 0; i < sum_messages.length; i++) {
+    if (sum_messages[i].attachments.size > 0)
       output.push(sum_messages[i]);
   }
 
@@ -112,146 +114,162 @@ async function getMessagesWithImages(channel, limit = 500) {
 client.on("message", async message => {
 
   var responses = ['It is certain.',
-                    'It is decidedly so.',
-                    'Without a doubt.',
-                    'Yes - definitely.',
-                    'You may rely on it.',
-                    'As I see it, yes.',
-                    'Most likely.',
-                    'Outlook good.',
-                    'Yes.',
-                    'Signs point to yes.',
-                    'Reply hazy, try again.',
-                    'Ask again later.',
-                    'Better not tell you now.',
-                    'Cannot predict now.',
-                    'Concentrate and ask again.',
-                    "Don't count on it.",
-                    'My reply is no.',
-                    'My sources say no.',
-                    'Outlook not so good',
-                    'Very doubtful'];
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes - definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    "Don't count on it.",
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good',
+    'Very doubtful'
+  ];
 
   // reminders for thirst command
 
   var reminders = ['A friendly reminder to stay hydrated.',
-                   'Quench your thirst.',
-                   'Did you drink enough water today?',
-                   'BEGONE',
-                   'stfu',
-                   'u thirsty hoe',
-                   'It is important to drink 8 glasses of water a day.',
-                   "goddammit i'm running out of creative ways to insult you people"];
+    'Quench your thirst.',
+    'Did you drink enough water today?',
+    'BEGONE',
+    'stfu',
+    'u thirsty hoe',
+    'It is important to drink 8 glasses of water a day.',
+    "goddammit i'm running out of creative ways to insult you people"
+  ];
 
 
   // Counting game stuff
-  if(message.channel.id === '698313651186040923') {
-      message.channel.messages.fetch({ limit: 2 }).then(messages => {
+  if (message.channel.id === '698313651186040923') {
+    message.channel.messages.fetch({
+      limit: 2
+    }).then(messages => {
 
-        // Delete bot messages
-        const lastMessage = messages.array();
-        if(lastMessage[0].author.bot || lastMessage[1].author.bot) {
-          message.delete(lastMessage[1]);
-          return;
-        }
-
-        // If past 2 messages sent by same user, delete newest
-        if(lastMessage[0].member.id == lastMessage[1].member.id) {
-          message.delete(lastMessage[1]);
-          return;
-        }
-
-        // If next number not old number + 1
-        if(Number(lastMessage[0].content).toString() !== lastMessage[0].content || Number(lastMessage[0].content) - 1 != Number(lastMessage[1].content)) {
-          // Add Can't Count role and delete last message if number isn't next in counting game and > 1500 ms between the two messages
-          if(Math.abs(lastMessage[0].createdTimestamp - lastMessage[1].createdTimestamp) > 1500) {
-            // Because Gloria is special
-            if(lastMessage[0].member.id !== '242067290479132675') {
-              const tMember = lastMessage[0].member.guild.roles.cache.find(role => role.name === "Can't Count");
-              lastMessage[0].member.roles.add(tMember).catch(console.error);
-            }
-          }
-
-          message.delete(lastMessage[0]);
-          return;
-        }
-      }).catch(err => {
-        console.error(err);
+      // Delete bot messages
+      const lastMessage = messages.array();
+      if (lastMessage[0].author.bot || lastMessage[1].author.bot) {
+        message.delete(lastMessage[1]);
         return;
-      })
+      }
 
+      // If past 2 messages sent by same user, delete newest
+      if (lastMessage[0].member.id == lastMessage[1].member.id) {
+        message.delete(lastMessage[1]);
+        return;
+      }
+
+      // If next number not old number + 1
+      if (Number(lastMessage[0].content).toString() !== lastMessage[0].content || Number(lastMessage[0].content) - 1 != Number(lastMessage[1].content)) {
+        // Add Can't Count role and delete last message if number isn't next in counting game and > 1500 ms between the two messages
+        if (Math.abs(lastMessage[0].createdTimestamp - lastMessage[1].createdTimestamp) > 1500) {
+          // Because Gloria is special
+          if (lastMessage[0].member.id !== '242067290479132675') {
+            const tMember = lastMessage[0].member.guild.roles.cache.find(role => role.name === "Can't Count");
+            lastMessage[0].member.roles.add(tMember).catch(console.error);
+          }
+        }
+
+        message.delete(lastMessage[0]);
+        return;
+      }
+    }).catch(err => {
+      console.error(err);
       return;
+    })
+
+    return;
   }
 
   // Don't respond to bots
-  if(message.author.bot)
+  if (message.author.bot)
     return;
-
 
   const command = message.content.toLowerCase();
 
   let override = false;
 
+  // Make sure message starts with 'queen'
   if (command.startsWith("queen"))
     override = true;
 
-  if(command.match(/\busercount\b/) != null) {
-    const userAmnt = client.guilds.cache.get('654783232969277450').memberCount;
-    message.channel.send("There are currently " + userAmnt + " people in this server");
-    console.log(client.guilds.cache.get('654783232969277450').memberCount);
-    return;
-  }
-
-  if(override) {
-    if(command.match(/\bhackathon\b/) != null) {
-      message.channel.send({files: ['https://cdn.discordapp.com/attachments/654784388197908500/675113678856781834/Screenshot_20200102-213727_Discord.png']});
+  if (override) {
+    if (command.match(/\busercount\b/) != null) {
+      const userAmnt = client.guilds.cache.get('654783232969277450').memberCount;
+      message.channel.send("There are currently " + userAmnt + " people in this server");
+      console.log(client.guilds.cache.get('654783232969277450').memberCount);
       return;
-    } else if(command.match(/\bgc\b/) != null) {
-      message.channel.send({files: ['https://cdn.discordapp.com/attachments/669726484772159488/701247357001400370/unknown.png']});
+    } else if (command.match(/\bhackathon\b/) != null) {
+      message.channel.send({
+        files: ['https://cdn.discordapp.com/attachments/654784388197908500/675113678856781834/Screenshot_20200102-213727_Discord.png']
+      });
       return;
-    } else if(command.match(/\bhead\b/) != null) {
-      message.channel.send({files: ['https://cdn.discordapp.com/attachments/669726484772159488/708103493918916709/unknown.png']});
+    } else if (command.match(/\bgc\b/) != null) {
+      message.channel.send({
+        files: ['https://cdn.discordapp.com/attachments/669726484772159488/701247357001400370/unknown.png']
+      });
       return;
-    } else if(command.match(/\bno anime\b/) != null) {
-      message.channel.send({files: ['https://cdn.discordapp.com/attachments/697639057592811650/708536846531035226/image0.jpg']});
+    } else if (command.match(/\bhead\b/) != null) {
+      message.channel.send({
+        files: ['https://cdn.discordapp.com/attachments/669726484772159488/708103493918916709/unknown.png']
+      });
       return;
-    } else if(command.match(/\bcontribute\b/) != null) {
+    } else if (command.match(/\bno anime\b/) != null) {
+      message.channel.send({
+        files: ['https://cdn.discordapp.com/attachments/697639057592811650/708536846531035226/image0.jpg']
+      });
+      return;
+    } else if (command.match(/\bcontribute\b/) != null) {
       message.channel.send("https://github.com/s-hfarooq/QueenMoonBot");
       return;
-    } else if(command.match(/\bmatt\b/) != null) {
-      message.channel.send({files: ['https://cdn.discordapp.com/attachments/654784481340686346/709596915485900890/IMG_9784.jpg']});
+    } else if (command.match(/\bmatt\b/) != null) {
+      message.channel.send({
+        files: ['https://cdn.discordapp.com/attachments/654784481340686346/709596915485900890/IMG_9784.jpg']
+      });
       return;
-    } else if(command.match(/\bquote\b/) != null) {
-        if (!(message.channel.id === '669726484772159488' || message.channel.id === '654784430409252904')) {
+    } else if (command.match(/\bquote\b/) != null) {
+      if (!(message.channel.id === '669726484772159488' || message.channel.id === '654784430409252904')) {
 
-          getMessagesWithImages(client.channels.cache.get("697329980044083220")).then(output => {
+        getMessagesWithImages(client.channels.cache.get("697329980044083220")).then(output => {
           let rand = Math.floor(Math.random() * output.length);
-          message.channel.send({files: [output[rand].attachments.first().url]});});
-        } else {
-            message.channel.send("That command cannot be used in this channel!");
-        }
+          message.channel.send({
+            files: [output[rand].attachments.first().url]
+          });
+        });
+      } else {
+        message.channel.send("That command cannot be used in this channel!");
       }
-    }else if(command.match(/\bbrownout\b/) != null) {
-      if(message.channel.id === '697639057592811650') {
+    } else if (command.match(/\bbrownout\b/) != null) {
+      if (message.channel.id === '697639057592811650') {
         getMessagesWithImages(client.channels.cache.get("697639057592811650")).then(output => {
           let rand = Math.floor(Math.random() * output.length);
-          message.channel.send({files: [output[rand].attachments.first().url]});
+          message.channel.send({
+            files: [output[rand].attachments.first().url]
+          });
         });
       } else {
         message.channel.send("That command can only be used in <#697639057592811650>");
       }
-    } else if(command.match(/\bhelp\b/) != null) {
+    } else if (command.match(/\bhelp\b/) != null) {
       message.channel.send("Commands:\n```* `queen no anime` to get the no anime picture\n* `queen hackathon` to get the done with hackathons picture\n* `queen gc` to get the Facebook group screenshot\n* `queen quote` to get a random image from #quotes\n* `queen head` to get the Mater screenshot\n* `queen usercount` to see how many users are currently in the server\n* `queen contribute` to get a like to the GitHub repo\n* `queen 8ball [message]` to get an 8ball reply (only works in #spam)\n* `queen thirst` to get water messages\n* `queen brownout` to get a random attachment from #brownoutposting (only works in #brownoutposting)```");
-    } else if(command.match(/\b8ball\b/) != null) {
-      if(message.channel.id === '654838387160907777') {
+    } else if (command.match(/\b8ball\b/) != null) {
+      if (message.channel.id === '654838387160907777') {
         var rand = Math.floor(Math.random() * responses.length);
         message.channel.send("Question: " + message.content.substring(12) + "\nAnswer: " + responses[rand]);
       } else {
         message.channel.send("That command can only be used in <#654838387160907777>");
       }
-    } else if(command.match(/\bthirst\b/) !=null) {
-        var rand = Math.floor(Math.random() * reminders.length);
-        message.channel.send(reminders[rand]);
+    } else if (command.match(/\bthirst\b/) != null) {
+      var rand = Math.floor(Math.random() * reminders.length);
+      message.channel.send(reminders[rand]);
     }
   }
 });
