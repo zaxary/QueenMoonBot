5a4faa86cab455144f6d36af23aca465761c38f5
Hassan F <1hassanfarooq@gmail.com>
2020-05-17 17:51:39 -0700

    cleaned up index.js

--- index.js
+++ index.js
@@ -4,61 +4,12 @@ const fs = require('fs');
 const count = require('./count.json');
 const config = require("./config.json");
 
-//CSV parser
-// const csv = require('csv-parser');
-//CSV data (filled later on)
-// var allCSVdata = [];
-// var classes = [];
-// var gpaData = [];
-
 var brownoutOut = [];
 var quotesOut = [];
 
 const client = new Discord.Client({
     partials: ['MESSAGE']
 });
-
-
-//initialize global map of courses
-// var courseCache = {
-//   ex001: ["Example Name", "0 hours", "4.0", "Example Description"],
-//   bus101: ["Professional Responsibility and Business", "3 hours", "3.85", "Introduces business students to professional responsibility. Develops the concept of professional responsibility within a personal and interpersonal context. Continues by expanding the concept to encompass the firm and explore the global corporate context. Introduces business majors and career paths and provides an understanding of ethical decision-making. Encourages the development of a professional identity and skills, preparing students to represent the College and the University with integrity and confidence in their careers. Prerequisite: First Semester Freshman, Intercollegiate and Off-Campus Transfer Students."]
-// };
-
-
-//function to read CSV data
-// function getCSVdata(){
-//   //get classes data from CSV file
-//   var classes = []
-//   fs.createReadStream('2020-fa.csv')
-//      .pipe(csv())
-//      .on('data', (row) => {
-//        classes.push(row);
-//        //console.log(row);
-//      })
-//      .on('end', () => {
-//        console.log('CSV processed.')
-//      })
-//   //get gpa data from CSV file
-//   var gpaData = []
-//   fs.createReadStream('uiuc-gpa-dataset.csv')
-//      .pipe(csv())
-//      .on('data', (row) => {
-//        gpaData.push(row);
-//        //console.log(row);
-//      })
-//      .on('end', () => {
-//        console.log('CSV processed.')
-//      })
-//   //return data as an array of arrays
-//   var data = []
-//   data.push(classes);
-//   data.push(gpaData);
-//   return data;
-// }
-
-
-
 
 // Run on start
 client.on("ready", () => {
@@ -73,13 +24,6 @@ client.on("ready", () => {
     });
 
     console.log("images cached");
-
-    //set CSV data variables
-    // console.log("Reading CSV data...")
-    // allCSVdata = getCSVdata();
-    // classes = allCSVdata[0];
-    // gpaData = allCSVdata[1];
-    // console.log("ALL CSV DATA IS NOW SAVED!");
 });
 
 // Runs on join new server
@@ -94,59 +38,6 @@ client.on("guildDelete", guild => {
     client.user.setActivity(`queen help`);
 });
 
-
-// let announcements_channel_id = "654785609335832586";
-// let committed_id = "683065503300059179";
-// let admitted_id = "683067243566858250";
-// let prospective_id = "654786754657910815";
-// let traitor_id = "696956653844561931";
-// let bot_channel_id = "654783232969277453";
-
-
-// client.on("messageReactionAdd",(reaction,user)=>{
-//   console.log("START REACT");
-
-//   if(!user) return;
-//   if(user.bot)return;
-//   if(!reaction.message.channel.guild) return;
-//   if(!reaction.message.channel.id === announcements_channel_id) return;
-//   member = reaction.message.guild.member(user)
-//   console.log(member.id);
-//   if (!member.roles.cache.some(r => r.name === "Prospective Student")  && !member.roles.cache.some(r => r.name === "Admitted Student")){
-
-//     return;
-//   }
-
-//   let committed_role = member.guild.roles.cache.find(r => r.id === committed_id);
-//   let admitted_role = member.guild.roles.cache.find(r => r.id === admitted_id);
-//   let prospective_role = member.guild.roles.cache.find(r => r.id === prospective_id);
-//   let traitor_role = member.guild.roles.cache.find(r => r.id === traitor_id);
-
-//   console.log("USER CREDENTIALS PASS");
-//   if(reaction.emoji.name == 'blockI') {
-//     console.log("blocki react");
-//     member.roles.add(committed_role);
-
-//     member.roles.remove(admitted_role).then(console.log).catch(console.error);
-//     member.roles.remove(prospective_role).then(console.log).catch(console.error);
-
-//     // if (!member.roles.cache.some(r => r.id === "654828722050039808") &&
-//     //     !member.roles.cache.some(r => r.id === "654828844133384222") &&
-//     //     !member.roles.cache.some(r => r.id === "654828941072400393") &&
-//     //     !member.roles.cache.some(r => r.id === "655176104251883561")) {
-//     //   member.user.send("Don't forget to request your roles in #set_roles_here!\n**ex: Admitted, Grainger, OOS, ElecEng @ESC**").catch(console.error);
-//     //   return;
-//     // }
-//   }
-//   if(reaction.emoji.name == 'traitor'){
-//     console.log("traitor react");
-
-//     member.roles.add(traitor_role);
-//     member.roles.remove(admitted_role).then(console.log).catch(console.error);
-//     member.roles.remove(prospective_role).then(console.log).catch(console.error);
-//   }
-// });
-
 async function getMessagesWithImages(channel, limit = 500) {
     const sum_messages = [];
     let last_id;
@@ -155,27 +46,26 @@ async function getMessagesWithImages(channel, limit = 500) {
     const options = {
       limit: 100
     };
-    if (last_id) {
+
+    if (last_id)
       options.before = last_id;
-    }
 
     const messages = await channel.messages.fetch(options);
     sum_messages.push(...messages.array());
     last_id = messages.last().id;
 
-    if (messages.size != 100 || sum_messages >= limit) {
+    if (messages.size != 100 || sum_messages >= limit)
       break;
-    }
   }
-
 
   const output = [];
   for (let i = 0; i < sum_messages.length; i++) {
-    if (sum_messages[i].attachments.size > 0)
+    // Only keep messages with attachments and messages not sent by bots
+    if (sum_messages[i].attachments.size > 0 && !sum_messages[i].author.bot)
       output.push(sum_messages[i]);
   }
 
-    return output;
+  return output;
 }
 
 // Runs when a new message is sent on a server
@@ -355,84 +245,6 @@ client.on("message", async message => {
       }
     } else if (command.match(/\bclass\b/) != null) {
       message.channel.send("That command has been disabled. Use class bot instead.");
-      //ex: queen class cs-125
-
-      //check for course in dictionary/map
-      // var words = command.replace('queen class ', '').split("-");
-      // var classCode = words[0].toLowerCase()+words[1];
-      // if (classCode in courseCache){
-      //   //notify on console
-      //   console.log("Course in cache. Fetching...");
-      //   message.channel.send(words[0].toUpperCase()+"-"+words[1]+": "+courseCache[classCode][0]+" -- "+courseCache[classCode][1]+" -- Offered in Fall 2020 -- Recent Avg. GPA: "+courseCache[classCode][2]);
-      //   message.channel.send("Description: "+courseCache[classCode][3]);
-      //   console.log("Course fetched.");
-      // }
-      // else{
-      //   try{
-      //     //notify on console
-      //     console.log("Course not in cache. Fetching...");
-      //     //splits into subject code & number
-      //     var words = command.split("-");
-      //     var userMessage = words[0].replace('queen class ', '');
-      //     subject = userMessage.toUpperCase();
-      //     classNum = words[1];
-      //     //get class info
-      //     var className = "N/A";
-      //     var credits = 0;
-      //     var classDescription = "";
-      //     for (var i = 0; i < classes.length; i++){
-      //       //console.log(classes[i]);
-      //       //console.log(classes[i].Subject);
-      //       if (classes[i].Subject == subject && classes[i].Number == classNum){
-      //         //set name of class
-      //         className = classes[i].Name;
-      //         //set credit hour amount
-      //         credits = classes[i].Credit;
-      //         //set description
-      //         classDescription = classes[i].Description;
-      //       }
-      //     }
-      //     //recent avg. GPA (only counts 2019 classes)
-      //     var gpa = 0;
-      //     //select relevant classes for calculation
-      //     var relevantClassesForGPA = [];
-      //     for (var i = 0; i < gpaData.length; i++){
-      //       if (gpaData[i].Subject == subject && gpaData[i].Number == classNum && gpaData[i].Year == 2019){
-      //         relevantClassesForGPA.push(gpaData[i]);
-      //       }
-      //     }
-      //     //calculate gpa
-      //     var totalGPA = 0;
-      //     var sumGPAs = 0;
-      //     for (var i = 0; i < relevantClassesForGPA.length; i++){
-      //       c = relevantClassesForGPA[i];
-      //       var gpasum = parseFloat(c.Aplus)+parseFloat(c.Aa)+parseFloat(c.Aminus)+parseFloat(c.Bplus)+parseFloat(c.Bb)+parseFloat(c.Bminus)+parseFloat(c.Cplus)+parseFloat(c.Cc)+parseFloat(c.Cminus)+parseFloat(c.Dplus)+parseFloat(c.Dd)+parseFloat(c.Dminus);
-      //       var gpatotal = ((parseFloat(c.Aplus) + parseFloat(c.Aa))*4)+(parseFloat(c.Aminus)*3.67)+(parseFloat(c.Bplus)*3.33)+(parseFloat(c.Bb)*3)+(parseFloat(c.Bminus)*2.67)+(parseFloat(c.Cplus)*2.33)+(parseFloat(c.Cc)*2)+(parseFloat(c.Cminus)*1.67)+(parseFloat(c.Dplus)*1.33)+(parseFloat(c.Dd*1))+(parseFloat(c.Dminus)*0.67);
-      //       totalGPA += gpatotal;
-      //       sumGPAs += gpasum;
-      //     }
-      //     gpa = totalGPA / sumGPAs;
-      //     //send response (only if class name isn't 'N/A', as this would indicate that it couldn't be found in the CSV)
-      //     if (className != "N/A"){
-      //       message.channel.send(userMessage+"-"+classNum+": "+className+" -- "+credits+" -- Offered in Fall 2020 -- Recent Avg. GPA: "+Number(gpa.toFixed(2)));
-      //       message.channel.send("Description: "+classDescription);
-      //       //message.channel.send("Dev Data: "+totalGPA+", Total GPA & "+sumGPAs+" GPA's...");
-      //       console.log("Course fetched.");
-      //
-      //       //SAVE TO MAP:
-      //       var courseData = [className, credits, Number(gpa.toFixed(2)).toString(), classDescription];
-      //       courseCache[classCode] = courseData;
-      //       //notify console
-      //       console.log("Course has been cached.");
-      //     }
-      //     else{
-      //       message.channel.send("Unfortunately, I wasn't able to find: "+userMessage+"-"+classNum+". This may mean that this course is not available in Fall 2020.");
-      //     }
-      //   }
-      //   catch(error){
-      //     message.channel.send("Sorry, that isn't the proper format for a class. Please use SUBJECT-NUMBER (ex. CS-125).")
-      //   }
-      // }
     } else if (command.match(/\bthirst\b/) != null) {
       var rand = Math.floor(Math.random() * reminders.length);
       message.channel.send(reminders[rand]);
